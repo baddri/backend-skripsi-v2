54d490d497dd72dfb5d15358d061f309edb7fde4
baddri <badri.ula@gmail.com>
2021-03-27 02:08:00 +0700

    ✔ add validation in createuserargs

--- src/api/user/args/createuser.args.ts
+++ src/api/user/args/createuser.args.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ArgsType, ObjectType } from '@nestjs/graphql';
+import { IsEmail, MinLength } from 'class-validator';
 
 @ArgsType()
 export class CreateUserArgs {
+  @IsEmail()
   @Field()
   public email: string;
 
+  @MinLength(8)
   @Field()
   public password: string;
 
